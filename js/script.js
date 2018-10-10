783393b49f60f1149c3e5fbe6b679ee6569d1511
Arthur <kondra.arthur98@gmail.com>
2018-10-10 16:15:54 +0000

    Initial

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
 menu.onclick = function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if(x.className === "topnav") {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
         x.className += " responsive";
-    } else{
+    }
+    else {
         x.className = "topnav";
     }
- }
+}
+
+function zoom(element) {
+    var newTab = window.open();
+    var data = document.getElementById("choicedPhoto").getAttribute("src");
+    setTimeout(function() {
+        newTab.document.body.innerHTML = "<img src='" + data + "'>";
+    }, 500);
+    return false;
+}
+
+$(function() {
+
+    $('.signup').on('click', '.fa', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('fa-square-o').toggleClass('fa-check-square-o');
+        if ($(this).hasClass('fa-square-o')) {
+            $('.subscribed').animate({
+                height: '0'
+            })
+        }
+        else if ($(this).hasClass('fa-check-square-o')) {
+            $('.subscribed').animate({
+                height: '75px'
+            })
+        }
+
+    })
+
+});
